39bfc1701a9a2bafa03894d719b0ad5e8ca61978
Leslie Chan <leslie.chan@live.hk>
2020-11-20 23:31:37 -0800

    Updates

--- component---src-pages-services-insurance-protection-js-682f677780652ea981b1.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{d5OJ:function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),o=a.n(n),r=a("8aQE"),c=a("Kvkj");t.default=function(e){var t=e.location,a=r.data[t.pathname.split("/").filter(Boolean).slice(-1)[0]];return o.a.createElement(c.m,{pageContext:a},o.a.createElement(c.r,{pageContext:a}))}}}]);
-//# sourceMappingURL=component---src-pages-services-insurance-protection-js-682f677780652ea981b1.js.map
