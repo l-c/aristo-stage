f3d704f8676b623bf5d2d2c68a9fbe2b5157caa4
JuicyBB <joyce.leung@live.hk>
2020-11-24 21:00:31 -0800

    Updates

--- component---src-pages-contact-js-183d7e212aa793a12cfa.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{"Cuy+":function(t,e,n){"use strict";n.r(e);var a=n("q1tI"),c=n.n(a),o=n("Bl7J"),i={url:"/contact",title:"Contact Us",description:"Contact Us",template:"other"};e.default=function(){return c.a.createElement(o.a,{pageContext:i},c.a.createElement("main",null,i.title))}}}]);
-//# sourceMappingURL=component---src-pages-contact-js-183d7e212aa793a12cfa.js.map
---
+++ component---src-pages-services-investment-strategies-js-20b2f66a520c37bfd644.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[13],{Vnu3:function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),o=a.n(n),r=a("8aQE"),c=a("Kvkj");t.default=function(e){var t=e.location,a=r.data[t.pathname.split("/").filter(Boolean).slice(-1)[0]];return o.a.createElement(c.m,{pageContext:a},o.a.createElement(c.r,{pageContext:a}))}}}]);
+//# sourceMappingURL=component---src-pages-services-investment-strategies-js-20b2f66a520c37bfd644.js.map
