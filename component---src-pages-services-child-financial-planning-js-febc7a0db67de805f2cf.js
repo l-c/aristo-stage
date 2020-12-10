d260e2c562d4345ef870f192eccfb920bd8ff3ec
JuicyBB <joyce.leung@live.hk>
2020-12-09 23:56:21 -0800

    Updates

---
+++ component---src-pages-services-child-financial-planning-js-febc7a0db67de805f2cf.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{BTLa:function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),o=a.n(n),c=a("8aQE"),i=a("Kvkj");t.default=function(e){var t=e.location,a=c.data[t.pathname.split("/").filter(Boolean).slice(-1)[0]];return o.a.createElement(i.o,{pageContext:a},o.a.createElement(i.w,{pageContext:a}))}}}]);
+//# sourceMappingURL=component---src-pages-services-child-financial-planning-js-febc7a0db67de805f2cf.js.map
